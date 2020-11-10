0324ca673dc92bdabec59617d94ad24f4624948b
eliasneri <eliasneri@hotmail.com>
2020-11-10 15:39:15 -0300

    Trabalhando com 'classes'

---
+++ functions.h
@@ -0,0 +1,102 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_YELLOW "\033[1;33m"
+
+int op=0;
+char c,senha[50], nome_user[10];
+
+void cabecalho(){
+printf("\nSeja Bem Vindo (id): ");
+printf(ANSI_COLOR_YELLOW "%s", nome_user);
+printf(ANSI_COLOR_RESET "\n");
+asteriscos(45);
+espaco();
+printf("\nCADASTRO DE PACIENTES POSITIVOS PARA COVID-19\n");
+asteriscos(45);
+}
+
+void asteriscos(int n){
+    for (int i=1; i<=n; i++)
+        printf("*");
+}
+
+void linhas(){
+    for (int j=1; j<=10; j++){
+            printf("\n");
+        }
+}
+
+void espaco(){
+for (int i=1; i<=30; i++){
+        printf(" ");
+     }
+}
+
+void erroid(){
+linhas();
+espaco();
+    for (int i=0; i<26; i++)
+    printf("*");
+printf("\n");
+espaco();
+printf(ANSI_COLOR_RED "*** ID/SENHA INCORRETA ***\n"ANSI_COLOR_RESET);
+espaco();
+for (int i=0; i<26; i++)
+    printf("*");
+}
+
+int validasenha(){
+do{
+
+           linhas();
+           int a=0;
+           fflush(stdin);
+           espaco();
+           printf("Digite o Usuário (id): ");
+           gets(nome_user);
+           fflush(stdin);
+           espaco();
+           printf("Digite a senha: ");
+           do{
+               c=getch();
+               if(isprint(c)){
+                    senha[a]=c;
+                    a++;
+                    printf("*");
+               }
+               else if(c==8&&a){
+                    senha[a]='\0';
+                    a--;
+                    printf("\b \b");
+                    }
+           }while(c!=13);
+
+
+           senha[a]='\0';
+           system("cls");
+           char scorreta[]="abcd";
+           char user[]="admin";
+
+
+           if (!strcmp(senha,scorreta) && (nome_user, user)) {
+                op=1;
+                break;
+                }
+           else {
+                erroid();
+                printf("\n\n");
+                system("pause");
+                system("cls");
+           }
+
+    } while (op==0);
+
+     return op;
+}
+
+
+
+
+#endif // FUNCTIONS_H_INCLUDED
--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include<stdlib.h>
 #include<string.h>
 #include <locale.h>
+#include "functions.h"
 #define ANSI_COLOR_RED      "\x1b[31m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 #define ANSI_COLOR_YELLOW "\033[1;33m"
 
-int op=0;
-char c,senha[50], nome_user[10];
-
 main(){
 
     setlocale(LC_ALL, "Portuguese");
-    //validasenha();
+    validasenha();
     system("cls");
+    gravarRegistro();
     cabecalho();
 
     int opcao=0;
@@ -29,20 +28,9 @@ main(){
 
     } while (opcao !=2);
 
-
-
-
 }
 
-void cabecalho(){
-printf("\nSeja Bem Vindo (id): ");
-printf(ANSI_COLOR_YELLOW "%s", nome_user);
-printf(ANSI_COLOR_RESET "\n");
-asteriscos(45);
-espaco();
-printf("\nCADASTRO DE PACIENTES POSITIVOS PARA COVID-19\n");
-asteriscos(45);
-}
+
 
 void gravarRegistro(){
     FILE *PTRARQ;
@@ -50,90 +38,13 @@ void gravarRegistro(){
     PTRARQ = fopen("ARQTXT01.txt", "a");
     printf("\n\nEscreva uma palavra: ");
     scanf("%s", PALAVRA);
-    fprintf(PTRARQ, "\n%s", PALAVRA);
+    fprintf(PTRARQ, "\n\n");
+    fprintf(PTRARQ, "***************************************************\n");
+    fprintf(PTRARQ, "%s\n", PALAVRA);
+    fprintf(PTRARQ, "***************************************************");
     fclose(PTRARQ);
     return 0;
 }
 
 
-void asteriscos(int n){
-    for (int i=1; i<=n; i++)
-        printf("*");
-}
-
-void linhas(){
-    for (int j=1; j<=10; j++){
-            printf("\n");
-        }
-}
-
-void espaco(){
-for (int i=1; i<=30; i++){
-        printf(" ");
-     }
-}
-
-void erroid(){
-linhas();
-espaco();
-    for (int i=0; i<26; i++)
-    printf("*");
-printf("\n");
-espaco();
-printf(ANSI_COLOR_RED "*** ID/SENHA INCORRETA ***\n"ANSI_COLOR_RESET);
-espaco();
-for (int i=0; i<26; i++)
-    printf("*");
-
-}
-
-
-int validasenha(){
-do{
-
-           linhas();
-           int a=0;
-           fflush(stdin);
-           espaco();
-           printf("Digite o Usuário (id): ");
-           gets(nome_user);
-           fflush(stdin);
-           espaco();
-           printf("Digite a senha: ");
-           do{
-               c=getch();
-               if(isprint(c)){
-                    senha[a]=c;
-                    a++;
-                    printf("*");
-               }
-               else if(c==8&&a){
-                    senha[a]='\0';
-                    a--;
-                    printf("\b \b");
-                    }
-           }while(c!=13);
-
-
-           senha[a]='\0';
-           system("cls");
-           char scorreta[]="abcd";
-           char user[]="admin";
-
-
-           if (!strcmp(senha,scorreta) && (nome_user, user)) {
-                op=1;
-                break;
-                }
-           else {
-                erroid();
-                printf("\n\n");
-                system("pause");
-                system("cls");
-           }
-
-    } while (op==0);
-
-     return op;
-}
 
